a98a79e5a191f98e6e7ca0fec3704e8916565790
SparkProPol <stephane.pn77@gmail.com>
2026-04-04 17:27:23 +0200

    Update grilles.js

--- content/data/grilles.js
+++ content/data/grilles.js
@@ -57,28 +57,138 @@ bc_cs: {
         echelons: {1:478,2:489,3:508,4:518,5:528,6:548,7:565}
       }
     },
+administratif: {
+
+  // CAT C
+  adjoint: {
+    label: "Adjoint administratif",
+    echelons: {1:366,2:375,3:385,4:395,5:405}
+  },
+
+  adjoint_principal_2: {
+    label: "Adjoint administratif principal 2e classe",
+    echelons: {1:385,2:395,3:405,4:415,5:425}
+  },
+
+  adjoint_principal_1: {
+    label: "Adjoint administratif principal 1re classe",
+    echelons: {1:405,2:415,3:425,4:435,5:445}
+  },
+
+  // CAT B
+  secretaire_cn: {
+    label: "Secrétaire administratif (classe normale)",
+    echelons: {1:390,2:410,3:430,4:450,5:470}
+  },
+
+  secretaire_cs: {
+    label: "Secrétaire administratif (classe supérieure)",
+    echelons: {1:470,2:490,3:510,4:530,5:550}
+  },
+
+  secretaire_exceptionnel: {
+    label: "Secrétaire administratif (classe exceptionnelle)",
+    echelons: {1:550,2:570,3:590}
+  },
+
+  // CAT A
+  attache: {
+    label: "Attaché",
+    echelons: {1:444,2:470,3:500,4:530,5:560}
+  },
+
+  attache_principal: {
+    label: "Attaché principal",
+    echelons: {1:600,2:640,3:680}
+  },
+
+  attache_hors_classe: {
+    label: "Attaché hors classe",
+    echelons: {1:700,2:750,3:800}
+  },
+
+  caiom: {
+    label: "CAIOM",
+    echelons: {1:830,2:890,3:950}
+  }
+
+},
+
+pts: {
+
+  // TECHNIQUE
+  adjoint_technique: {
+    label: "Adjoint technique",
+    echelons: {1:366,2:375,3:385,4:395}
+  },
+
+  adjoint_technique_principal: {
+    label: "Adjoint technique principal",
+    echelons: {1:385,2:395,3:405,4:415}
+  },
+
+  controleur: {
+    label: "Contrôleur technique",
+    echelons: {1:410,2:430,3:450,4:470}
+  },
+
+  ingenieur_technique: {
+    label: "Ingénieur technique",
+    echelons: {1:500,2:550,3:600}
+  },
+
+  // SCIENTIFIQUE (PTS)
+  agent_pts: {
+    label: "Agent spécialisé PTS",
+    echelons: {1:366,2:375,3:385}
+  },
+
+  technicien_pts: {
+    label: "Technicien PTS",
+    echelons: {1:400,2:420,3:440,4:460}
+  },
+
+  technicien_principal_pts: {
+    label: "Technicien principal PTS",
+    echelons: {1:460,2:480,3:500}
+  },
+
+  ingenieur_pts: {
+    label: "Ingénieur PTS",
+    echelons: {1:500,2:550,3:600}
+  },
+
+  ingenieur_principal_pts: {
+    label: "Ingénieur principal PTS",
+    echelons: {1:600,2:650,3:700}
+  }
+
+},
+    retraite: {
+
+  taux: 0.75, // taux plein fonction publique (75%)
+
+  decote: 0.0125, // 1,25% par trimestre manquant
+  surcote: 0.0125, // 1,25% par trimestre supplémentaire
+
+  // 🔥 minimum garanti (approximation)
+  minimum: 1200,
+
+  // 🔥 coefficients selon catégorie (utile futur)
+  categories: {
+
+    actif: {
+      bonification: 0.2 // exemple : bonification 1/5e (à affiner)
+    },
 
     administratif: {
-      adjoint: {
-        label: "Adjoint administratif",
-        echelons: {1:366,2:375,3:385}
-      },
-      secretaire: {
-        label: "Secrétaire administratif",
-        echelons: {1:390,2:410,3:430}
-      }
+      bonification: 0
     },
 
     pts: {
-      technicien: {
-        label: "Technicien PTS",
-        echelons: {1:400,2:420,3:440}
-      }
-    },
-
-    retraite: {
-      taux: 0.75
+      bonification: 0
     }
 
   }
-};
+
+}
