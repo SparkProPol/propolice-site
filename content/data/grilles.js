a2ab7dcfe8ffa0344eb02b8fe5db6ca8528374c0
SparkProPol <stephane.pn77@gmail.com>
2026-04-04 17:11:25 +0200

    Update grilles.js

--- content/data/grilles.js
+++ content/data/grilles.js
@@ -43,7 +43,23 @@ const BDD = {
       }
     },
 
-   crs: "actif",
+   crs: JSON.parse(JSON.stringify({
+  gardien: {
+    label: "Gardien de la paix",
+    issp: 0.285,
+    echelons: {1:366,2:376,3:388,4:401,5:415,6:431,7:448,8:466}
+  },
+  brigadier_chef: {
+    label: "Brigadier-chef",
+    issp: 0.285,
+    echelons: {1:483,2:499,3:515,4:532,5:548,6:565}
+  },
+  major: {
+    label: "Major",
+    issp: 0.285,
+    echelons: {1:478,2:489,3:508,4:518,5:528,6:548,7:565}
+  }
+}))
 
     administratif: {},
 
