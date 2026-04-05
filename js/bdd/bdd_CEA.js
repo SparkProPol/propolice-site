7abe75c189ba2a59cfde1e60c292b9e311504b21
SparkProPol <stephane.pn77@gmail.com>
2026-04-05 20:53:18 +0200

    Update bdd_CEA.js

--- js/bdd/bdd_CEA.js
+++ js/bdd/bdd_CEA.js
@@ -1,1 +1,35 @@
+const BDD_CEA = {
 
+  valeur_point: 4.92278,
+
+  zones: { 0:0, 1:0.03, 2:0.01 },
+
+  actif: {
+
+    gardien: {
+      label: "Gardien de la paix",
+      issp: 0.20,
+      echelons: {1:366,2:376,3:388,4:401,5:415,6:431,7:448,8:466}
+    },
+
+    bc_cn: {
+      label: "Brigadier-chef (classe normale)",
+      issp: 0.22,
+      echelons: {1:483,2:499,3:515,4:532,5:548}
+    },
+
+    bc_cs: {
+      label: "Brigadier-chef (classe supérieure)",
+      issp: 0.22,
+      echelons: {1:565,2:585,3:605}
+    },
+
+    major: {
+      label: "Major",
+      issp: 0.24,
+      echelons: {1:478,2:489,3:508,4:518,5:528,6:548,7:565}
+    }
+
+  }
+
+};
