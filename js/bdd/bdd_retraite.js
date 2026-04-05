cfa180c71714ad1a4430539b673f5a157f5bb558
SparkProPol <stephane.pn77@gmail.com>
2026-04-05 20:54:45 +0200

    Update bdd_retraite.js

--- js/bdd/bdd_retraite.js
+++ js/bdd/bdd_retraite.js
@@ -1,1 +1,14 @@
+const BDD_RETRAITE = {
 
+  taux: 0.75,
+  decote: 0.0125,
+  surcote: 0.0125,
+  minimum: 1200,
+
+  categories: {
+    actif: { bonification: 0.2 },
+    administratif: { bonification: 0 },
+    pts: { bonification: 0 }
+  }
+
+};
