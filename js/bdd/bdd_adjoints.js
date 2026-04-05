6d5fa326cd17a12dc93bda80e2b9d9d5fd6b1f5e
SparkProPol <stephane.pn77@gmail.com>
2026-04-05 20:55:04 +0200

    Update bdd_adjoints.js

--- js/bdd/bdd_adjoints.js
+++ js/bdd/bdd_adjoints.js
@@ -1,1 +1,15 @@
+const BDD_ADJOINTS = {
 
+  statut: "Policier adjoint",
+
+  salaire: {
+    base: 1450,
+    evolution: [1450, 1500, 1550]
+  },
+
+  primes: {
+    nuit: 2,
+    dimanche: 6
+  }
+
+};
