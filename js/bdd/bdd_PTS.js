7915f25ebf315e8b289a4b21d8943caf53ea73c5
SparkProPol <stephane.pn77@gmail.com>
2026-04-05 20:54:21 +0200

    Update bdd_PTS.js

--- js/bdd/bdd_PTS.js
+++ js/bdd/bdd_PTS.js
@@ -1,1 +1,52 @@
+const BDD_PTS = {
 
+  pts: {
+
+    adjoint_technique: {
+      label: "Adjoint technique",
+      echelons: {1:366,2:375,3:385,4:395}
+    },
+
+    adjoint_technique_principal: {
+      label: "Adjoint technique principal",
+      echelons: {1:385,2:395,3:405,4:415}
+    },
+
+    controleur: {
+      label: "Contrôleur technique",
+      echelons: {1:410,2:430,3:450,4:470}
+    },
+
+    ingenieur_technique: {
+      label: "Ingénieur technique",
+      echelons: {1:500,2:550,3:600}
+    },
+
+    agent_pts: {
+      label: "Agent spécialisé PTS",
+      echelons: {1:366,2:375,3:385}
+    },
+
+    technicien_pts: {
+      label: "Technicien PTS",
+      echelons: {1:400,2:420,3:440,4:460}
+    },
+
+    technicien_principal_pts: {
+      label: "Technicien principal PTS",
+      echelons: {1:460,2:480,3:500}
+    },
+
+    ingenieur_pts: {
+      label: "Ingénieur PTS",
+      echelons: {1:500,2:550,3:600}
+    },
+
+    ingenieur_principal_pts: {
+      label: "Ingénieur principal PTS",
+      echelons: {1:600,2:650,3:700}
+    }
+
+  }
+
+};
