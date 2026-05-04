2b503e37fd8ad1e50435d89269fac02a17dad409
SparkProPol <stephane.pn77@gmail.com>
2026-05-04 15:58:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -679,27 +679,9 @@ function exportCMOPDF() {
 window.addEventListener("DOMContentLoaded", () => {
   updateEchelonMax();
   remplirEchelons();
-});;
+});
 
-document.getElementById("echelon")?.addEventListener("input", function () {
-
-  const grade = document.getElementById("grade").value;
-
-  const maxEchelons = {
-    gpx: 12,
-    bc_norm: 8,
-    bc_sup: 7,
-    major: 7
-  };
-
-  const max = maxEchelons[grade] || 12;
-
-  if (parseInt(this.value, 10) > max) {
-    this.value = max;
-  }
-
-  if (parseInt(this.value, 10) < 1) {
-    this.value = 1;
-  }
-
+document.getElementById("grade")?.addEventListener("change", () => {
+  updateEchelonMax();
+  remplirEchelons();
 });
