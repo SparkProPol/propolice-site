990d9360575fb542182b17bf3df1e93ae162715c
SparkProPol <stephane.pn77@gmail.com>
2026-05-05 16:04:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -571,6 +571,13 @@ const ICSS = (corps === "CRS") ? 145 : 0;
   const bloc = `
   <div style="display:grid; gap:10px;">
     <div class="row between"><span>Salaire de base valorisé</span><strong>${salaireBase.toFixed(2)} €</strong></div>
+    ${typeZone === "metropole" ? `
+<div class="row between"><span>Indemnité de résidence</span><strong>+ ${IR.toFixed(2)} €</strong></div>
+` : ""}
+
+${typeZone === "outremer" ? `
+<div class="row between"><span>Majoration Outre-mer</span><strong>+ ${majorationOM.toFixed(2)} €</strong></div>
+` : ""}
     <div class="row between"><span>ISSP (28,5%)</span><strong>+ ${ISSP.toFixed(2)} €</strong></div>
     ${corps === "CRS" ? `
 <div class="row between"><span>ICSS CRS</span><strong>+ ${ICSS.toFixed(2)} €</strong></div>
