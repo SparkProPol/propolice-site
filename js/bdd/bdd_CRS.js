44b70a14072259f74a3bf0135ae30f4ad411bd32
SparkProPol <stephane.pn77@gmail.com>
2026-05-15 20:27:27 +0200

    Update bdd_CRS.js

--- js/bdd/bdd_CRS.js
+++ js/bdd/bdd_CRS.js
@@ -7,22 +7,18 @@ const BDD_CRS = {
   actif: {
 
     gpx: {
-      label: "Gardien de la paix CRS",
       echelons: [375,377,381,387,401,415,424,439,458,475,493,511,532]
     },
 
-    bcn: { // Brigadier-chef classe normale
-      label: "Brigadier-chef",
+    bcn: {
       echelons: [410,421,436,453,470,478,489,508]
     },
 
-    bcs: { // Brigadier-chef classe supérieure
-      label: "Brigadier-chef supérieur",
+    bcs: {
       echelons: [478,489,508,518,528,548,565]
     },
 
     major: {
-      label: "Major CRS",
       echelons: [515,535,549,567,579,615,620,640,660,690]
     }
 
